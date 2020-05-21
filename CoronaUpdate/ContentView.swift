82e1979112c093f07d8d15a051cf93d1880e4b95
Ufuoma Okoro <ufookoro@gmail.com>
2020-05-21 20:34:24 +0100

    Initial Commit

---
+++ CoronaUpdate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaUpdate
+//
+//  Created by Ufuoma Okoro on 21/05/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
